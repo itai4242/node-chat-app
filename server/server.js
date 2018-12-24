70ebd0795df3134d195e658577ff0f12eafc39cd
itai4242 <itai4230@gmail.com>
2018-12-24 13:11:06 +0200

    emit new message on createmessage

--- server/server.js
+++ server/server.js
@@ -13,11 +13,11 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'John',
+  //   text: 'See you then',
+  //   createdAt: 123123
+  // });
 
   // socket.emit('newEmail', {
   //   from: 'mike@example.com',
@@ -31,6 +31,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from:message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
