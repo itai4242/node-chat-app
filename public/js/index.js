790a7dfda8ac5328b47e2b90294dc570844f1fe4
itai4242 <itai4230@gmail.com>
2018-12-27 14:58:16 +0200

    add geolocation via api

--- public/js/index.js
+++ public/js/index.js
@@ -28,6 +28,17 @@ socket.on('newMessage', function (message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a trget = "_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
+
+
 // socket.emit('createMessage', {
 //   from: 'Frank',
 //   text: 'hi'
@@ -45,3 +56,18 @@ jQuery('#message-form').on('submit', function (e) {
 
   });
 });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser')
+  }
+  navigator.geolocation.getCurrentPosition(function (posiotion) {
+    socket.emit('createLocationMessage',{
+      latitude:posiotion.coords.latitude,
+      longitude:posiotion.coords.longitude,    
+    });
+  }, function () {
+    alert('Unable to fetch location');
+  })
+})
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage}= require('./message');
+var {generateMessage, generateLocationMessage}= require('./message');
 
 describe('generateMessage',()=>{
   it('should generatecorrect message object',()=>{
@@ -8,6 +8,19 @@ describe('generateMessage',()=>{
     var res = generateMessage(from, text);
     expect(res.from).toBe(from);
     expect(res.text).toBe(text) ;
+    expect(typeof res.createdAt).toBe('number');
+  })
+})
+
+describe('generateLocationMessage',()=>{
+  it('should generate correct location object',()=>{
+    var from = 'itai';
+    var lat =15;
+    var long = 19;
+    var adress =`https://www.google.com/maps/?q=15,19`
+    var res = generateLocationMessage(from, lat,long);
+    expect(res.from).toBe(from);
+    expect(res.url).toBe(adress) ;
     expect(typeof res.createdAt).toBe('number');
   })
 })
